3a8e748c27a4e5ac4c5659e9171e9e42cc302f52
nguyenb12 <nguyennbrian123@gmail.com>
2025-02-08 01:46:41 -0500

    Added about section implementation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { Navbar } from "./compnents/sections/Navbar";
 import { MobileMenu } from "./compnents/MobileMenu";
 import { Home } from "./compnents/sections/Home";
+import { About } from "./compnents/sections/About";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -12,14 +13,15 @@ function App() {
 
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}  {""}
-      <div className={`min-h-screen transition-opacity duraction-700 
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-opacity duration-700 
         ${isLoaded ? "opacity-100" : "opacity-0"} 
         bg-black text-gray-100`}
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
+        <About />
       </div>
     </>
   );
--- src/compnents/sections/Home.jsx
+++ src/compnents/sections/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
             className="min-h-screen flex flex-col items-center justify-center"
         >
             <div className="text-center z-10 px-4">
-                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-blue-400 text-transparent bg-clip-text">
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 text-transparent bg-clip-text">
                     Hi, I'm Brian
                 </h1>
                 <p className="text-gray-400 text-lg mb-8 max-w-lg max-auto">
@@ -17,6 +17,19 @@ export const Home = () => {
                     and I am always eager to take on new challenges that push me to grow as a developer
 
                 </p>
+                <div className="flex justify-center space-x-4">
+                    <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition 
+                    relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59, 130, 246, 0.4)]"
+                    >
+                        View Projects
+                    </a>
+                    <a href="#contact"
+                        className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
+                        hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgb(59, 130, 246, 0.2) hover:bg-blue-500/10]"
+                    >
+                        Contact Me
+                    </a>
+                </div>
 
             </div>
 
