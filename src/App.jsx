69950d51626ed5e0c306374cdefe7091ceffc685
nguyenb12 <nguyennbrian123@gmail.com>
2025-02-07 21:28:58 -0500

    Added some styling; Says name at the top right. as well as some styling for loading page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./compnents/LoadingScreen"; // Corrected typo in `components`
 import "./index.css";
+import { Navbar } from "./compnents/sections/Navbar";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
 
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}  {""}
+      <div className={`min-h-screen transition-opacity duraction-700 
+        ${isLoaded ? "opacity-100" : "opacity-0"} 
+        bg-black text-gray-100`}
+      >
+        <Navbar />
+      </div>
     </>
   );
 }
