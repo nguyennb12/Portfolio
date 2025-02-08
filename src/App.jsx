ee741e8fc0ff43bd6f5faf3e158e22ad15418396
nguyenb12 <nguyennbrian123@gmail.com>
2025-02-07 21:14:48 -0500

    Touched up on the front page loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
-import { LoadingScreen } from './compnents/LoadingScreen'
-
+import { useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./compnents/LoadingScreen"; // Corrected typo in `components`
+import "./index.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <>
-      <LoadingScreen />
-
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
     </>
   );
-
 }
-export default App
+
+export default App;
