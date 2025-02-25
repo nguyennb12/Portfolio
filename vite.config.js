4ad21d402a7e4a2504fc476b0a80b0e6488cb976
nguyenb12 <nguyennbrian123@gmail.com>
2025-02-25 00:06:03 -0500

    Updated portfolio  for gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://nguyennb12.github.io/Portfolio/',
   plugins: [react(), tailwindcss()],
 });
